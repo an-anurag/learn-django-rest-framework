dc3183fb4cdac7bbb8b47b0874b46a96e449e693
Anurag <an.anurag@msn.com>
2019-01-14 01:53:23 +0530

    Nested serialization support with update api

--- blog/class_api/serializers.py
+++ blog/class_api/serializers.py
@@ -7,6 +7,7 @@ from ..models import Post, Movie, Song
 
 
 class SongSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
 
     class Meta:
         model = Song
@@ -18,6 +19,8 @@ class SongSerializer(serializers.ModelSerializer):
             'duration',
         )
 
+        # create got multiple keyword argument for movie TypeError
+        read_only_fields = ('movie',)
         # depth attribute for read only ie. for get
         # depth = 1
 
@@ -38,14 +41,49 @@ class MovieSerializer(serializers.ModelSerializer):
         # depth = 1
 
     def create(self, validated_data):
+        # validated data is dict
         print(validated_data)
+        # pop song because it is not a field of movie
         songs = validated_data.pop('songs')
+        # create a movie object
         movie = Movie.objects.create(**validated_data)
+        # songs is ordered dict list
         print(songs)
         for song in songs:
             print(song)
+            # pick a each dict then create song object associated with movie created earlier
+
+            # create got multiple keyword argument for movie TypeError (read_only_fields)
+            # movie_id=movie.id
             Song.objects.create(**song, movie_id=movie.id)
         return movie
+
+    def update(self, instance, validated_data):
+        # validated data is dict
+        print(validated_data)
+        # pop song because it is not a field of movie
+        songs = validated_data.pop('songs')
+        instance.name = validated_data.get("name", instance.name)
+        instance.save()
+        keep_songs = []
+        existing_ids = [song.id for song in instance.songs]
+        for song in songs:
+            if 'id' in song.keys():
+                if Song.objects.filter(id=song['id']).exists():
+                    s = Song.objects.get(id=song['id'])
+                    s.title = song.get('title', song.title)
+                    s.save()
+                    keep_songs.append(song.id)
+                else:
+                    continue
+            else:
+                s = Song.objects.create(**song, movie=instance)
+                keep_songs.append(s.id)
+        
+        for song in instance.songs:
+            if song.id not in keep_songs:
+                song.delete()
+        return instance
 
 
 class PostSerializer(serializers.ModelSerializer):
