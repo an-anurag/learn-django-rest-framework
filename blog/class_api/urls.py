8d8a4fd9478f9857081a32c82dae3b27c6590654
Anurag <an.anurag@msn.com>
2019-02-12 01:23:54 +0530

    myapp added

--- blog/class_api/urls.py
+++ blog/class_api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('posts/', views.PostAPIView.as_view(), name='list'),
     path('posts/<int:id>/', views.PostDetailView.as_view(), name='detail'),
     path('movies/', views.MovieListAPIView.as_view(), name='movie_list'),
-    path('movie/<int:id>/', views.MovieDetailAPIView.as_view(), name='movie_detail'),
+    path('movies/<int:id>/', views.MovieDetailAPIView.as_view(), name='movie_detail'),
     path('songs/', views.SongAPIView.as_view(), name='song_list'),
 
 ]
